11b5922c549b0fb41d6198fe4b68c69c380ecffe
Chris Saylor <cjsaylor@gmail.com>
2019-06-09 13:40:19 -0400

    Protect against going over maximum width

--- src/baluster.js
+++ src/baluster.js
@@ -1,12 +1,12 @@
 'use strict'
 
-// Minimum industry required width between balusters
-const MINIMUM_WIDTH = 4
+// Maximum industry required width between balusters
+const MAXIMUM_WIDTH = 4
 
 const BALUSTER_2X2 = 1.75
 
 function estimateQuantity(widthBetweenPosts, balusterWidth = BALUSTER_2X2) {
-	return Math.floor(widthBetweenPosts / (MINIMUM_WIDTH + balusterWidth))
+	return Math.floor(widthBetweenPosts / (MAXIMUM_WIDTH + balusterWidth))
 }
 
 function spacingMeasurements(widthBetweenPosts, balusterQuantity, balusterWidth = BALUSTER_2X2) {
@@ -21,11 +21,14 @@ function spacingMeasurements(widthBetweenPosts, balusterQuantity, balusterWidth 
 				acc[i - 1] + spacing + balusterWidth
 			]
 		}, [])
-	// .map(val => [Math.floor(val), [nearest16th(val), 16]])
 }
 
 function calculateSpacing(widthBetweenPosts, balusterQuantity, balusterWidth = BALUSTER_2X2) {
-	return (widthBetweenPosts - (balusterQuantity * balusterWidth)) / (balusterQuantity + 1)
+	const spacing = (widthBetweenPosts - (balusterQuantity * balusterWidth)) / (balusterQuantity + 1)
+	if (spacing > MAXIMUM_WIDTH) {
+		throw "Too few balusters for given width."
+	}
+	return spacing
 }
 
 function nearest16th(decimal) {
