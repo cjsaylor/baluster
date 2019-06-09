82a7c5ca39fc4852bf800a7048b4ac7d53137dbe
Chris Saylor <cjsaylor@gmail.com>
2019-06-09 13:39:50 -0400

    Update config to support node and web.
    Includes a dev build as well.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,27 @@
 const path = require('path');
 
-module.exports = {
+module.exports = [{
+	mode: 'production',
+	target: 'web',
+	entry: './src/baluster.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'baluster.min.js',
+		library: 'baluster',
+		libraryTarget: 'umd',
+		umdNamedDefine: true,
+		globalObject: 'typeof self !== \'undefined\' ? self : this',
+	},
+}, {
+	mode: 'development',
+	target: 'web',
 	entry: './src/baluster.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'baluster.js',
 		library: 'baluster',
 		libraryTarget: 'umd',
-	}
-};
+		umdNamedDefine: true,
+		globalObject: 'typeof self !== \'undefined\' ? self : this',
+	},
+}]
