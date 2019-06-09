0944dbb86928d1cd262e2c0324b6cc36fbe3853f
Chris Saylor <cjsaylor@gmail.com>
2019-06-09 13:41:50 -0400

    Latest build.

--- dist/baluster.js
+++ dist/baluster.js
@@ -1,1 +1,112 @@
-!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports.baluster=t():e.baluster=t()}(window,function(){return function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}return n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t,n){"use strict";const r=4,o=1.75;function u(e,t,n=o){return(e-t*n)/(t+1)}e.exports={estimateQuantity:function(e,t=o){return Math.floor(e/(r+t))},spacingMeasurements:function(e,t,n=o){const r=u(e,t,n);return[...Array(t).keys()].reduce((e,t)=>t?[...e,e[t-1]+r+n]:[r+n/2],[])},calculateSpacing:u,nearest16th:function(e){return Math.round(16*(e-Math.floor(e)))}}}])});
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define("baluster", [], factory);
+	else if(typeof exports === 'object')
+		exports["baluster"] = factory();
+	else
+		root["baluster"] = factory();
+})(typeof self !== 'undefined' ? self : this, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/baluster.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/baluster.js":
+/*!*************************!*\
+  !*** ./src/baluster.js ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\n// Maximum industry required width between balusters\nconst MAXIMUM_WIDTH = 4\n\nconst BALUSTER_2X2 = 1.75\n\nfunction estimateQuantity(widthBetweenPosts, balusterWidth = BALUSTER_2X2) {\n\treturn Math.floor(widthBetweenPosts / (MAXIMUM_WIDTH + balusterWidth))\n}\n\nfunction spacingMeasurements(widthBetweenPosts, balusterQuantity, balusterWidth = BALUSTER_2X2) {\n\tconst spacing = calculateSpacing(widthBetweenPosts, balusterQuantity, balusterWidth)\n\treturn [...Array(balusterQuantity).keys()]\n\t\t.reduce((acc, i) => {\n\t\t\tif (!i) {\n\t\t\t\treturn [spacing + balusterWidth / 2]\n\t\t\t}\n\t\t\treturn [\n\t\t\t\t...acc,\n\t\t\t\tacc[i - 1] + spacing + balusterWidth\n\t\t\t]\n\t\t}, [])\n}\n\nfunction calculateSpacing(widthBetweenPosts, balusterQuantity, balusterWidth = BALUSTER_2X2) {\n\tconst spacing = (widthBetweenPosts - (balusterQuantity * balusterWidth)) / (balusterQuantity + 1)\n\tif (spacing > MAXIMUM_WIDTH) {\n\t\tthrow \"Too few balusters for given width.\"\n\t}\n\treturn spacing\n}\n\nfunction nearest16th(decimal) {\n\treturn Math.round((decimal - Math.floor(decimal)) * 16)\n}\n\nmodule.exports = {\n\testimateQuantity,\n\tspacingMeasurements,\n\tcalculateSpacing,\n\tnearest16th\n}\n\n//# sourceURL=webpack://baluster/./src/baluster.js?");
+
+/***/ })
+
+/******/ });
+});
